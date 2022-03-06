89bf4d8726267f9b3d6968a91504d835a1382db7
22969541+Aurelius333@users.noreply.github.com <22969541+Aurelius333@users.noreply.github.com>
2022-03-06 10:27:21 -0500

    CritterController, reproduction

---
+++ Assets/CritterController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CritterController : MonoBehaviour
+{
+    public int consumed, hydrated, stepSize;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        var gameObject = collision.gameObject;
+        if (gameObject.CompareTag("Critter"))
+        {
+            var otherController = gameObject.GetComponent<CritterController>();
+
+            if (consumed > 230 && hydrated > 230 && otherController.consumed > 230 && otherController.hydrated > 230)
+            {
+                consumed -= 100;
+                hydrated -= 100;
+
+                otherController.consumed -= 100;
+                otherController.hydrated -= 100;
+
+                // make baby
+                Instantiate(gameObject);
+            }
+        }
+    }
+}
--- Assets/Manager.cs
+++ Assets/Manager.cs
@@ -2,19 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Controller : MonoBehaviour
+public class Manager : MonoBehaviour
 {
-    Genetics g = new Genetics();
+    public GameObject critterPrefab;
 
     // Start is called before the first frame update
     void Start()        
     {
-        g.setup();
+        Instantiate(critterPrefab, new Vector3(3, 1.5f, 3), Quaternion.identity);
     }
 
     // Update is called once per frame
     void Update()
     {
-        g.draw();
+        
     }
 }
