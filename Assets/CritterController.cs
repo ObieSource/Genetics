e94ebb236e42ee41de2ef9e51ef85a9973ab0b39
22969541+Aurelius333@users.noreply.github.com <22969541+Aurelius333@users.noreply.github.com>
2022-03-06 23:38:37 -0500

    Mostly working

--- Assets/CritterController.cs
+++ Assets/CritterController.cs
@@ -2,15 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using System.Linq;
+
 public class CritterController : MonoBehaviour
 {
     public float consumed, hydrated;
     public float consumptionLostPerSecond;
     public float hydrationLostPerSecond;
+    public float force;
+
+    public GameObject target;
+    private Rigidbody rb;
 
     void Start()
     {
         gameObject.GetComponent<Renderer>().material.color = Color.red;
+
+        rb = gameObject.GetComponent<Rigidbody>();
     }
 
     void Update()
@@ -21,6 +29,16 @@ public class CritterController : MonoBehaviour
         if (consumed <= 0 || hydrated <= 0)
         {
             Die();
+        }
+
+        target = ChooseTarget();
+
+        if (target)
+        {
+            Vector3 forceToMove = target.transform.position - transform.position;
+            forceToMove.y = 0;
+            forceToMove.Normalize();
+            rb.AddForce(forceToMove * force);
         }
     }
 
@@ -53,6 +71,57 @@ public class CritterController : MonoBehaviour
                 Instantiate(gameObject);
             }
         }
+    }
+
+    private GameObject ChooseTarget()
+    {
+        if (consumed > 230 && hydrated > 230)
+        {
+            GameObject closestCritter = Closest(GameObject.FindGameObjectsWithTag("Critter"));
+            if (closestCritter) return closestCritter;
+        }
+
+        GameObject closestFood = Closest(GameObject.FindGameObjectsWithTag("Food"));
+        GameObject closestWater = Closest(GameObject.FindGameObjectsWithTag("Water"));
+
+        if (closestFood == null) return closestWater;
+        if (closestWater == null) return closestFood;
+
+        // do whatever is the most worthwhile
+        if (DistanceTo(closestFood) / consumed < DistanceTo(closestWater) / hydrated)
+        {
+            return closestFood;
+        }
+        else
+        {
+            return closestWater;
+        }
+    }
+
+    private GameObject Closest(GameObject[] gameObjects)
+    {
+        GameObject closest = null;
+        float closestDistance = -1;
+        foreach (GameObject obj in gameObjects)
+        {
+            float dist = DistanceTo(obj);
+            if (obj != this.gameObject && (closestDistance == -1 || dist < closestDistance))
+            {
+                closest = obj;
+                closestDistance = dist;
+            }
+        }
+        return closest;
+    }
+
+    private float DistanceTo(GameObject other)
+    {
+        return DistanceBetween(gameObject, other);
+    }
+
+    private static float DistanceBetween(GameObject o1, GameObject o2)
+    {
+        return Vector3.Distance(o1.transform.position, o2.transform.position);
     }
 
     private void Die()
